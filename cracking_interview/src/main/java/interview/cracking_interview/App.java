848ed64b72c48be475e103a455afa7199bf3728d
jojo <chiranjeeb.nandy@yahoo.com>
2015-12-27 12:20:06 -0600

    Added initial code.

---
+++ cracking_interview/src/main/java/interview/cracking_interview/App.java
@@ -0,0 +1,13 @@
+package interview.cracking_interview;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
