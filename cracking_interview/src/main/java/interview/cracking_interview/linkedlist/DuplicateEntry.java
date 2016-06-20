f20bfec3dd177dd87cd6f16d1b82b067a417f42a
jojo <chiranjeeb.nandy@yahoo.com>
2016-06-20 04:30:42 -0500

    linked list

---
+++ cracking_interview/src/main/java/interview/cracking_interview/linkedlist/DuplicateEntry.java
@@ -0,0 +1,37 @@
+package interview.cracking_interview.linkedlist;
+
+/**
+ * Remove duplicate nodes from linked list without using buffer
+ * @author jojo
+ *
+ */
+public class DuplicateEntry
+{
+	//no buffer
+	public Node removeDuplicate(Node root){
+		Node current = root.next, prev = root;
+		
+		while(current != null){
+			prev = root;
+			while(prev !=current){
+				if(prev.val == current.val){
+					prev.next = prev.next.next;
+					prev = prev.next;
+					break;
+				}
+			}
+			
+			current = current.next;
+		}
+		
+		return root;
+	}
+	
+	private static class Node{
+		public int val;
+		public Node next;
+		public Node(int val){
+			this.val = val;
+		}
+	}
+}
